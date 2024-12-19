b15871b40a0639c3f6255dab176576984bb124f0
Emre Aksu <114534582+eemreaksu@users.noreply.github.com>
2024-12-19 21:19:46 +0300

    Add files via upload
    İlk 4 ders

---
+++ SELECT DISTNICT PEMDAS.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM parks_and_recreation.employee_demographics;
+
+select first_name,
+last_name,
+age,
+age + 10
+
+FROM parks_and_recreation.employee_demographics;
+
+SELECT  DISTINCT  gender,
+first_name
+
+FROM parks_and_recreation.employee_demographics;
